09dfeec7c2840bba3b58a7b151a097e3e56e5875
devallever <devallever@163.com>
2019-06-27 18:40:51 +0800

    内容： 天界基类handler

--- common/src/main/java/com/allever/lib/common/app/BaseActivity.kt
+++ common/src/main/java/com/allever/lib/common/app/BaseActivity.kt
@@ -1,10 +1,12 @@
 package com.allever.lib.common.app
 
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import com.allever.lib.common.util.DLog
 
 open class BaseActivity: AppCompatActivity() {
+    protected val mHandler = Handler()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         DLog.d(this.javaClass.simpleName)
