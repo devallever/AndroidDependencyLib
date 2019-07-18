01880df31e32b3e5d9020f01293144f0c0f7b466
devallever <devallever@163.com>
2019-07-18 16:55:59 +0800

    内容：添加日志toast扩展类

--- common/src/main/java/com/allever/lib/common/util/ExtFunction.kt
+++ common/src/main/java/com/allever/lib/common/util/ExtFunction.kt
@@ -1,13 +1,16 @@
 package com.allever.lib.common.util
 
-import android.util.Log
 import android.widget.Toast
 import com.allever.lib.common.app.App
+import com.allever.lib.common.util.log.LogUtils
+import org.jetbrains.anko.runOnUiThread
 
 fun Any.log(msg: String) {
-    Log.d("", msg)
+    LogUtils.d(msg)
 }
 
 fun Any.toast(msg: String) {
-    Toast.makeText(App.context, msg, Toast.LENGTH_SHORT).show()
+    App.context?.runOnUiThread {
+        Toast.makeText(App.context, msg, Toast.LENGTH_SHORT).show()
+    }
 }
