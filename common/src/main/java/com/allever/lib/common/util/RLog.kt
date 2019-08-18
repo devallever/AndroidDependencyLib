98b52faa503dbcf15f480fdf10c88e27f40b4ab6
devallever <devallever@163.com>
2019-08-18 21:41:11 +0800

    内容：提交代码

--- common/src/main/java/com/allever/lib/common/util/RLog.kt
+++ common/src/main/java/com/allever/lib/common/util/RLog.kt
@@ -6,17 +6,13 @@ object RLog {
 
     private val TAG = RLog::class.java.simpleName
 
-    fun d(msg: String) {
-        d(TAG, msg)
-    }
+    fun d(msg: String) = d(TAG, msg)
 
     fun d(tag: String, msg: String?) {
         Log.d(tag, msg)
     }
 
-    fun e(msg: String) {
-        e(TAG, msg)
-    }
+    fun e(msg: String) = e(TAG, msg)
 
     fun e(tag: String, msg: String?) {
         Log.e(tag, msg)
