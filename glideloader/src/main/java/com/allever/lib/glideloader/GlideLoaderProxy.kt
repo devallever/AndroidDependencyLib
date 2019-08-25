1e9d2b20e3a1bedd3fbf9f86052af33880c6e368
devallever <devallever@163.com>
2019-08-25 12:59:02 +0800

    内容：注释文件

--- glideloader/src/main/java/com/allever/lib/glideloader/GlideLoaderProxy.kt
+++ glideloader/src/main/java/com/allever/lib/glideloader/GlideLoaderProxy.kt
@@ -1,29 +1,29 @@
-package com.allever.lib.glideloader
-
-import android.content.Context
-import android.widget.ImageView
-import com.allever.lib.imageloader.ImageLoaderProxy
-import com.bumptech.glide.Glide
-import java.io.File
-
-class GlideLoaderProxy: ImageLoaderProxy {
-    override fun resumeRequest(context: Context) {
-        Glide.with(context).resumeRequests()
-    }
-
-    override fun stopRequest(context: Context) {
-        Glide.with(context).pauseRequests()
-    }
-
-    override fun loadImage(context: Context, url: String, imageView: ImageView) {
-        Glide.with(context).load(url).into(imageView)
-    }
-
-    override fun loadImage(context: Context, file: File, imageView: ImageView) {
-        Glide.with(context).load(file).into(imageView)
-    }
-
-    override fun loadImage(context: Context, resId: Int, imageView: ImageView) {
-        Glide.with(context).load(resId).into(imageView)
-    }
-}
+//package com.allever.lib.glideloader
+//
+//import android.content.Context
+//import android.widget.ImageView
+//import com.allever.lib.imageloader.ImageLoaderProxy
+//import com.bumptech.glide.Glide
+//import java.io.File
+//
+//class GlideLoaderProxy: ImageLoaderProxy {
+//    override fun resumeRequest(context: Context) {
+//        Glide.with(context).resumeRequests()
+//    }
+//
+//    override fun stopRequest(context: Context) {
+//        Glide.with(context).pauseRequests()
+//    }
+//
+//    override fun loadImage(context: Context, url: String, imageView: ImageView) {
+//        Glide.with(context).load(url).into(imageView)
+//    }
+//
+//    override fun loadImage(context: Context, file: File, imageView: ImageView) {
+//        Glide.with(context).load(file).into(imageView)
+//    }
+//
+//    override fun loadImage(context: Context, resId: Int, imageView: ImageView) {
+//        Glide.with(context).load(resId).into(imageView)
+//    }
+//}
