f04a8b9fed5899d6d35db565c52402edfeb192eb
devallever <devallever@163.com>
2019-08-27 00:04:16 +0800

    内容：优化友盟调用接口

--- umeng/src/main/java/com/allever/lib/umeng/UMeng.kt
+++ umeng/src/main/java/com/allever/lib/umeng/UMeng.kt
@@ -5,7 +5,11 @@ import com.umeng.analytics.MobclickAgent
 import com.umeng.commonsdk.UMConfigure
 
 object UMeng {
-    fun init(context: Context, appKey: String, channel: String = "google", auto: Boolean = true) {
+    /***
+     * @param appKey 如果在Manifest配置了，传null
+     * @param channel 如果在Manifest配置了，传null
+     */
+    fun init(context: Context, appKey: String? = null, channel: String? = null, auto: Boolean = true) {
         UMConfigure.init(context, appKey, channel, UMConfigure.DEVICE_TYPE_PHONE, null)
         if (auto) {
             MobclickAgent.setPageCollectionMode(MobclickAgent.PageMode.AUTO)
