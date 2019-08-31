bccef02b6b3217292a0051c2c2ee1ff90d01122a
devallever <devallever@163.com>
2019-08-31 11:29:11 +0800

    内容：优化友盟接口

--- umeng/src/main/java/com/allever/lib/umeng/UMeng.kt
+++ umeng/src/main/java/com/allever/lib/umeng/UMeng.kt
@@ -6,11 +6,21 @@ import com.umeng.commonsdk.UMConfigure
 
 object UMeng {
     /***
+     * init(this):  正式包：读取配置文件的key和channel
+     *              调试包：读取配置文件的key和channel默认为debug
      * @param appKey 如果在Manifest配置了，传null
      * @param channel 如果在Manifest配置了，传null
      */
     fun init(context: Context, appKey: String? = null, channel: String? = null, auto: Boolean = true) {
-        UMConfigure.init(context, appKey, channel, UMConfigure.DEVICE_TYPE_PHONE, null)
+        UMConfigure.setLogEnabled(BuildConfig.DEBUG)
+        val appChannel = if (BuildConfig.DEBUG) {
+            channel ?: "debug"
+        } else {
+            channel
+        }
+
+        UMConfigure.init(context, appKey, appChannel, UMConfigure.DEVICE_TYPE_PHONE, null)
+
         if (auto) {
             MobclickAgent.setPageCollectionMode(MobclickAgent.PageMode.AUTO)
         } else {
