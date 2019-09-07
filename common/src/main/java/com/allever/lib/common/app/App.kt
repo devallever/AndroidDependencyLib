906fcdb4d43f7cd240d35fa7b4ef708cb08b4e27
devallever <devallever@163.com>
2019-09-08 02:30:20 +0800

    内容：添加multidex

--- common/src/main/java/com/allever/lib/common/app/App.kt
+++ common/src/main/java/com/allever/lib/common/app/App.kt
@@ -3,9 +3,10 @@ package com.allever.lib.common.app
 import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
+import androidx.multidex.MultiDexApplication
 import com.allever.lib.common.util.log.LogUtils
 
-open class App : Application() {
+open class App : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
         context = this
