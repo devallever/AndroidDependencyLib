2626b164826c37612d731a46e51b9dd2562860de
devallever <devallever@163.com>
2019-11-05 00:34:57 +0800

    内容：优化分享

--- common/src/main/java/com/allever/lib/common/util/ShareHelper.kt
+++ common/src/main/java/com/allever/lib/common/util/ShareHelper.kt
@@ -2,22 +2,35 @@ package com.allever.lib.common.util
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import com.allever.lib.common.R
+import com.allever.lib.common.app.App
+import java.io.File
 
 object ShareHelper {
 
     fun shareText(obj: Any, msg: String) {
+        share(obj, getShareTextIntent(msg))
+    }
+
+    fun shareAudio(obj: Any, path: String) {
+        share(obj, getShareAudioIntent(path))
+    }
+
+    private fun share(obj: Any, intent: Intent) {
         try {
-            when(obj) {
+            when (obj) {
                 is Fragment -> {
-                    obj.startActivity(getShareIntent(msg))
+                    obj.startActivity(intent)
                 }
                 is android.app.Fragment -> {
-                    obj.startActivity(getShareIntent(msg))
+                    obj.startActivity(intent)
                 }
                 is Activity -> {
-                    obj.startActivity(getShareIntent(msg))
+                    obj.startActivity(intent)
                 }
             }
         } catch (e: Exception) {
@@ -26,11 +39,31 @@ object ShareHelper {
         }
     }
 
-    private fun getShareIntent(msg: String): Intent? {
+    private fun getShareTextIntent(msg: String): Intent {
         val shareIntent = Intent()
         shareIntent.action = Intent.ACTION_SEND
         shareIntent.putExtra(Intent.EXTRA_TEXT, msg)
         shareIntent.type = "text/plain"
+        return Intent.createChooser(
+            shareIntent,
+            getString(R.string.common_share_to)
+        )
+    }
+
+    private fun getShareAudioIntent(path: String): Intent {
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.type = "audio/*"
+
+        val file = File(path)
+        val fileUri: Uri
+        fileUri = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            //解决调用相册不显示图片的问题
+            FileProvider.getUriForFile(App.context, "${App.context.packageName}.fileprovider", file)
+        } else {
+            Uri.fromFile(file)
+        }
+
+        shareIntent.putExtra(Intent.EXTRA_STREAM, fileUri)
         return Intent.createChooser(
             shareIntent,
             getString(R.string.common_share_to)
