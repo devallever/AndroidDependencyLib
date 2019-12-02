8af6d49fe870325b99ee2ed904a272c8f56aa5c0
devallever <devallever@163.com>
2019-12-02 16:32:31 +0800

    内容：判断系统是否中文

--- common/src/main/java/com/allever/lib/common/util/SystemUtils.kt
+++ common/src/main/java/com/allever/lib/common/util/SystemUtils.kt
@@ -1,8 +1,14 @@
 package com.allever.lib.common.util
 
 import android.app.Activity
+import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.provider.MediaStore
+import android.util.Log
+import com.allever.lib.common.app.App
+import java.util.*
 
 object SystemUtils {
     /**
@@ -12,5 +18,31 @@ object SystemUtils {
         val albumIntent = Intent(Intent.ACTION_PICK)
         albumIntent.data = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
         activity.startActivityForResult(albumIntent, requestCode)
+    }
+
+    fun openUrl(activity: Activity, url: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        try {
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET)
+            log("Launching intent: " + intent + " with extras: " + intent.extras)
+            activity.startActivity(intent)
+        } catch (ignored: ActivityNotFoundException) {
+            log("Nothing available to handle $intent")
+        }
+
+    }
+
+    fun isChineseLang(): Boolean {
+        val configuration = App.context.resources.configuration
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            return configuration.locale.language == "zh"
+        } else {
+            val langList = configuration.locales
+            if (langList.isEmpty) {
+                return true
+            }
+            val lang = langList[0]
+            return lang.language == "zh"
+        }
     }
 }
