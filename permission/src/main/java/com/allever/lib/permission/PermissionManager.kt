bc8c4b278f59e6a5a5347bace62207d67d8d82c6
devallever <devallever@163.com>
2020-08-06 09:17:45 +0800

    OPTIMIZE: 优化权限库

--- permission/src/main/java/com/allever/lib/permission/PermissionManager.kt
+++ permission/src/main/java/com/allever/lib/permission/PermissionManager.kt
@@ -2,7 +2,6 @@ package com.allever.lib.permission
 
 import android.app.Activity
 import android.content.DialogInterface
-import androidx.fragment.app.Fragment
 import com.allever.lib.common.app.App
 import com.yanzhenjie.permission.AndPermission
 
@@ -24,24 +23,24 @@ object PermissionManager {
             .start()
     }
 
-    fun request(
-        activity: Activity?,
-        listener: PermissionListener,
-        vararg permissions: String
-    ) {
-        FastPermission.request(activity, listener, *permissions)
-    }
-
-    fun request(fragment: Fragment, listener: PermissionListener, vararg permissions: String) {
-        FastPermission.request(fragment, listener, *permissions)
-    }
+//    fun request(
+//        activity: Activity?,
+//        listener: PermissionListener,
+//        vararg permissions: String
+//    ) {
+//        FastPermission.request(activity, listener, *permissions)
+//    }
+//
+//    fun request(fragment: Fragment, listener: PermissionListener, vararg permissions: String) {
+//        FastPermission.request(fragment, listener, *permissions)
+//    }
 
     fun hasPermissions(vararg permissions: String): Boolean {
-        return FastPermission.hasPermissions(App.context, *permissions)
+        return PermissionCompat.hasPermission(App.context, *permissions)
     }
 
     fun alwaysDenyPermissions(activity: Activity, vararg permissions: String): Boolean =
-        FastPermission.hasAlwaysDeniedPermission(activity, *permissions)
+        AndPermission.hasAlwaysDeniedPermission(activity, *permissions)
 
     fun jumpPermissionSetting(
         activity: Activity?,
